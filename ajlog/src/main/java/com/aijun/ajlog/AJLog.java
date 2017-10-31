38185ff0dc27e8701e8ab570c3e8ffdbf87d25bb
aijunx <aijun198600@126.com>
2017-10-31 18:06:22 +0800

    源代码增加依赖情况

--- ajlog/src/main/java/com/aijun/ajlog/AJLog.java
+++ ajlog/src/main/java/com/aijun/ajlog/AJLog.java
@@ -1,6 +1,6 @@
 package com.aijun.ajlog;
 
-//import com.aijun.ajotherlog.AJOtherLog;
+import com.aijun.ajotherlog.AJOtherLog;
 
 /**
  * Created by Aijun on 2017/10/23.
@@ -9,8 +9,8 @@ package com.aijun.ajlog;
 public class AJLog {
 
     public static void log() {
-//        System.out.print(AJOtherLog.logMessage);
-        System.out.print("======AJLog");
+        System.out.print(AJOtherLog.logMessage);
+//        System.out.print("======AJLog");
     }
 
 }
